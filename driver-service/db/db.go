d34a188fa1fa721c885254ac2669561715db26e0
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2025-02-02 08:03:00 +0100

    Initialize db for driver service

---
+++ driver-service/db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Init() *mongo.Database {
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGO_URI environment variable not set")
+	}
+	clientOpts := options.Client().ApplyURI(mongoURI)
+	client, err := mongo.Connect(context.Background(), clientOpts)
+	if err != nil {
+		log.Fatal("Unable to connect to MongoDB:", err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal("Ping failed. Cannot connect to server:", err)
+	}
+
+	fmt.Println("Connection to MongoDB successful")
+	return client.Database("Data")
+}
