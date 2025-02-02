19848d7fb17e77268aac852e220f2e452ef558f8
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2025-02-02 23:53:05 +0100

    Add vehicle model

--- driver-service/internal/models/vehicle.go
+++ driver-service/internal/models/vehicle.go
@@ -3,9 +3,11 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Vehicle struct {
-	DriverID       primitive.ObjectID `json:"id" bson:"_id"`
-	Brand     string             `json:"brand" bson:"brand"`
+	ID       primitive.ObjectID `json:"id" bson:"_id"`
+	DriverID primitive.ObjectID `json:"driverID" bson:"_driverID"`
+	Brand    string             `json:"brand" bson:"brand"`
 	Model    string             `json:"model" bson:"model"`
-	Year string             `json:"year" bson:"year"`
-	Color string             `json:"color" bson:"color"`
+	Year     string             `json:"year" bson:"year"`
+	Color    string             `json:"color" bson:"color"`
+	RegNum   string				`json:"regnum" bson:"regnum"`
 }
