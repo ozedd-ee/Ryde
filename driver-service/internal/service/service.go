6617c68b71644797324c0873a8cc4cb74d73f536
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2025-02-03 00:03:55 +0100

    Add service layer for driver service

---
+++ driver-service/internal/service/service.go
@@ -0,0 +1,80 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"ryde/internal/data"
+	"ryde/internal/models"
+	"ryde/internal/utils"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type DriverService struct {
+	DriverStore *data.DriverStore
+	VehicleStore *data.VehicleStore
+}
+
+func NewDriverService(driverStore *data.DriverStore, vehicleStore *data.VehicleStore) *DriverService {
+	return &DriverService{
+		DriverStore: driverStore,
+		VehicleStore: vehicleStore,
+	}
+}
+
+func (s *DriverService) SignUp(ctx context.Context, driver *models.Driver) (*models.Driver, error) {
+	exists, err := s.DriverStore.GetDriverByEmail(ctx, driver.Email)
+	if err != nil && err != mongo.ErrNoDocuments {
+		return nil, err
+	} else if exists != nil {
+		return nil, errors.New("driver already exists")
+	}
+
+	hashedPassword, err := utils.HashPassword(driver.Password)
+	if err != nil {
+		return nil, err
+	}
+	driver.Password = hashedPassword
+	return s.DriverStore.CreateDriver(ctx, driver)
+}
+
+func (s *DriverService) Login(ctx context.Context, email, password string) (string, error) {
+	driver, err := s.DriverStore.GetDriverByEmail(ctx, email)
+	if err != nil {
+		return "", err
+	}
+
+	err = utils.CheckPasswordMatch(password, driver.Password)
+	if err != nil {
+		return "", errors.New("incorrect password")
+	}
+
+	token, err := utils.GenerateJWT(driver.ID.Hex())
+	if err != nil {
+		return "", errors.New("unable to generate JWT")
+	}
+	return token, nil
+}
+
+func (s *DriverService) GetDriver(ctx context.Context, driverID string) (*models.Driver, error) {
+	return s.DriverStore.GetDriver(ctx, driverID)
+}
+
+func (s *DriverService) AddVehicle(ctx context.Context, vehicle *models.Vehicle) (*models.Vehicle, error) {
+	exists, err := s.DriverStore.GetDriverByEmail(ctx, vehicle.RegNum)
+	if err != nil && err != mongo.ErrNoDocuments {
+		return nil, err
+	} else if exists != nil {
+		return nil, errors.New("vehicle already added")
+	}
+
+	return s.VehicleStore.AddVehicle(ctx, vehicle)
+}
+
+func (s *DriverService) GetVehicleDetails(ctx context.Context, driverID string) (*models.Vehicle, error) {
+	vehicle, err := s.VehicleStore.GetVehicleByDriverID(ctx, driverID)
+	if err != nil {
+		return nil, errors.New("no vehicle added")
+	}
+	return vehicle, nil
+}
