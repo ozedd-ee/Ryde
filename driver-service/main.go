9e7e2446c57f6e884de547a1f8b6d8a591ddbdc6
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2025-02-04 09:04:22 +0100

    Initialize all layers and run server

---
+++ driver-service/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"ryde/db"
+	"ryde/internal/controller"
+	"ryde/internal/data"
+	"ryde/internal/routes"
+	"ryde/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Initialize gin
+	router := gin.Default()
+	
+	// Initialize database
+	db := db.Init()
+
+	// Initialize all layers
+	driverStore := data.NewDriverStore(db)
+	vehicleStore := data.NewVehicleStore(db)
+	driverService := service.NewDriverService(driverStore, vehicleStore)
+	driverController := controller.NewDriverController(driverService)
+
+	// define driver routes
+	routes.DriverRoutes(router, driverController)
+
+	// start server
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
+}
