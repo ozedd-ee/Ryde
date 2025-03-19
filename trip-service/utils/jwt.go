f0461a2b507db507469087281055b08caf185118
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2025-03-19 11:51:55 +0100

    Add jwt validation

---
+++ trip-service/utils/jwt.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var jwtSecret = os.Getenv("SECRET_KEY") // For test purposes
+
+type Claims struct {
+	DriverID string `json:"_id"`
+	jwt.StandardClaims
+}
+
+func ValidateJWT(tokenString string) (*Claims, error) {
+	if jwtSecret == "" {
+		log.Fatal("Secret key not set")
+	}
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+	if err != nil || !token.Valid {
+		return nil, errors.New("invalid or expired token")
+	}
+
+	return claims, nil
+}
