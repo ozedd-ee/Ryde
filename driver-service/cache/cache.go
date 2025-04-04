51f47b891a2ca5a9ee01c16915440641986a3623
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2025-04-04 21:56:16 +0100

    Fetch URL from environment

--- driver-service/cache/cache.go
+++ driver-service/cache/cache.go
@@ -1,10 +1,19 @@
 package cache
 
-import "github.com/redis/go-redis/v9"
+import (
+	"log"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
 
 // Initialize Redis client
 func Init() *redis.Client {
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		log.Fatal("REDIS_URL environment variable not set")
+	}
 	return redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr: redisURL,
 	})
 }
