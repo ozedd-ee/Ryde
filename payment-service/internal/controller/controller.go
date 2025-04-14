adf4b27ced005cbe5359d1c73ddd62ce6a63fb5b
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2025-04-14 22:25:27 +0100

    Add controller for payment service

---
+++ payment-service/internal/controller/controller.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"net/http"
+	"ryde/internal/models"
+	"ryde/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+type PaymentController struct {
+	PaymentService *service.PaymentService
+}
+
+func NewPaymentController(paymentService *service.PaymentService) *PaymentController {
+	return &PaymentController{
+		PaymentService: paymentService,
+	}
+}
+
+func (pc *PaymentController) NewSubAccount(c *gin.Context) {
+	driverID := c.Param("driver-id")
+	var subAccountRequest models.SubAccountRequest
+	if err := c.ShouldBindJSON(&subAccountRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+	}
+	subAccountID, err := pc.PaymentService.CreateSubAccount(c.Request.Context(), driverID, &subAccountRequest)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+	}
+	c.JSON(http.StatusOK, subAccountID)
+}
